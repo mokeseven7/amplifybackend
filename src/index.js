7439d16fa16c5c498168e5d5b3e5ce14f32fc876
Mike McGrath <mike.mcgrath@rallysportdirect.com>
2021-01-04 00:12:24 -0700

    finish up before trying lambda version

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 //If this isnt first, nothing in .env will be available in process.env
 import { setup } from './config/setup';
 
-import { ApolloServer, gql } from 'apollo-server-express';
+import { ApolloServer, makeExecutableSchema} from 'apollo-server-express';
 import express from 'express';
 import mongoose from 'mongoose';
 import {typeDefs} from './typeDefs'
@@ -12,19 +12,25 @@ setup();
 
 
 const startServer = async () => {
-  
-  //Create the Web & GraphQL Servers
-  const app = express();
-  const server = new ApolloServer({ typeDefs, resolvers });
 
-  //Plugin for express to know how to handle graphql queries
+  //Create the websesrver
+  const app = express();
+  const port = process.env.PORT || 4000;
+
+  // Build GraphQL schema based on SDL definitions and resolvers maps
+  const server = new ApolloServer({ typeDefs, resolvers, introspection:true });
   server.applyMiddleware( { app } );
 
+  //Setup the database connection
   const uri = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_CLUSTER}/${process.env.MONGODB_DATABASE}?retryWrites=true&w=majority`;
   await mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true});
 
+
+  
+
   //Open the tcp connection
-  app.listen({ port: 4000 }, () =>
+  app.listen({ port }, () =>
+
   console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
 )}
 
